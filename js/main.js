a3dd06097c40baf6517c703ad3080c35daa0dd67
Sol <solroa088@gmail.com>
2024-10-26 15:26:10 -0300

    Agrego el javascript

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const cerrarFuera = document.querySelector("#cerrarFuera");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+cerrarFuera.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
